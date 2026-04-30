6f36e2768f69f823f5e64833c3e31cc432518a30
DILIP PRAJAPATI <dkprajapati9046@gmail.com>
2026-04-30 12:52:39 +0530

    requirements.txt

--- resume_analyzer/settings.py
+++ resume_analyzer/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^$b_ct)x-usd)nxq0rr12d+1o30z4h9i!=zou_q=x^3+=@9nng
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
